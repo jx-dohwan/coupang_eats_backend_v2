83636c4184000f1c12163906f2663251536437f9
jx-dohwan <jx7789@gmail.com>
2025-12-04 08:59:26 +0900

    [FEAT] 전역 미들웨어 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { CoreModule } from './core/core.module';
 import { RequestLoggerMiddleware } from './core/middleware/requestLogger.middleware';
 import { AuthModule } from './modules/auth/auth.module';
+import { Module } from '@nestjs/common';
+
+
 const applicationModules = [UserModule, AuthModule];
 
 @Module({
@@ -12,9 +14,5 @@ const applicationModules = [UserModule, AuthModule];
   controllers: [],
   providers: [],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    // 모든 요청에 대해 로그 미들웨어를 실행하라
-    consumer.apply(RequestLoggerMiddleware).forRoutes('*');
-  }
-}
+export class AppModule {}
+
