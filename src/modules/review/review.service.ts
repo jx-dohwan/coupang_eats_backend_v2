6fa8a987b67f6dae87fdd4f1c10cc06c27e139e6
jx-dohwan <jx7789@gmail.com>
2025-12-09 21:36:47 +0900

    [FEAT] added service

---
+++ src/modules/review/review.service.ts
@@ -0,0 +1,51 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { ReviewRepository } from './repository/review.repository';
+import { OrderRepository } from '../order/repository/order.repository';
+import { RestaurantRepository } from '../restaurant/repository/restaurant.repository';
+import { User } from '../../entities/user/user.entity';
+import { CreateReviewDto } from './dto/create-review.dto';
+import { OrderStatus } from '../../common/type/common.interface';
+
+@Injectable()
+export class ReviewService {
+  constructor(
+    private readonly reviewRepository: ReviewRepository,
+    private readonly orderRepository: OrderRepository,
+    private readonly restaurantRepository: RestaurantRepository,
+  ) {}
+
+  async createReview(user: User, dto: CreateReviewDto) {
+    // 1. 주문 조회
+    const order = await this.orderRepository.findByIdOrThrow(dto.orderId);
+
+    // 2. 권한 및 상태 검증
+    if (order.customerId !== user.id) {
+      throw new BadRequestException('You can only review your own orders.');
+    }
+    if (order.restaurantId !== dto.restaurantId) {
+      throw new BadRequestException('Order does not match restaurant.');
+    }
+    if (order.status !== OrderStatus.Delivered) {
+      throw new BadRequestException(
+        'You can only review after delivery is complete.',
+      );
+    }
+
+    // 3. 리뷰 저장
+    const restaurant = await this.restaurantRepository.findByIdOrThrow(
+      dto.restaurantId,
+    );
+
+    const review = await this.reviewRepository.save(
+      this.reviewRepository.create({
+        score: dto.score,
+        reviewText: dto.reviewText,
+        reviewImg: dto.reviewImg,
+        client: user,
+        restaurant: restaurant,
+      }),
+    );
+
+    return review;
+  }
+}
