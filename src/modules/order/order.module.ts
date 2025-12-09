a5d71d84d932378276c644064650cfe6da87068e
jx-dohwan <jx7789@gmail.com>
2025-12-09 20:49:23 +0900

    [FEAT] added module

---
+++ src/modules/order/order.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrderService } from './order.service';
+import { OrderController } from './order.controller';
+import { OrderRepository } from './repository/order.repository';
+import { OrderItemRepository } from './repository/order-item.repository';
+import { RestaurantModule } from '../restaurant/restaurant.module';
+import { DishModule } from '../dish/dish.module';
+import { OrderItemEntity } from '../../entities/order/order-item.entity';
+import { OrderEntity } from '../../entities/order/order.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([OrderEntity, OrderItemEntity]),
+    RestaurantModule,
+    DishModule,
+  ],
+  controllers: [OrderController],
+  providers: [OrderService, OrderRepository, OrderItemRepository],
+})
+export class OrderModule {}
