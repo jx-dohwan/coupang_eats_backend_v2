e57cb71c8283edb093cdbaae4c0c0d0bc8884142
jx-dohwan <jx7789@gmail.com>
2025-12-11 20:52:26 +0900

    [FEAT] dish Swagger 적용

--- src/modules/dish/dish.controller.ts
+++ src/modules/dish/dish.controller.ts
@@ -15,7 +15,15 @@ import { Role } from '../../entities/user/user.interface';
 import { CurrentUser } from '../../core/decorator/currentUser.decorator';
 import { User } from '../../entities/user/user.entity';
 import { CreateDishDto } from './dto/create-dish.dto';
+import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiDocCreated,
+  ApiDocOk,
+} from '../../core/decorator/swagger.decorator';
+import { DishEntity } from '../../entities/dish/dish.entity';
+import { CoreOutput } from '../../common/dto/core.output';
 
+@ApiTags('Dish (메뉴)')
 @Controller()
 export class DishController {
   constructor(private readonly dishService: DishService) {}
@@ -25,6 +33,7 @@ export class DishController {
    * URL: POST /restaurants/:restaurantId/dishes
    * 점주만 가능하며, Service 내부에서 본인 식당인지 한 번 더 체크함
    */
+  @ApiDocCreated('메뉴 생성', DishEntity)
   @Post('restaurants/:restaurantId/dishes')
   @UseGuards(AccessTokenGuard, RolesGuard)
   @Roles(Role.OWNER)
@@ -40,6 +49,7 @@ export class DishController {
    * 메뉴 삭제
    * URL: DELETE /dishes/:id
    */
+  @ApiDocOk('메뉴 삭제', CoreOutput)
   @Delete('dishes/:id')
   @UseGuards(AccessTokenGuard, RolesGuard)
   @Roles(Role.OWNER)
