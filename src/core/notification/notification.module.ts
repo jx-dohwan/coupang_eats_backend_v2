0d4c29ae3d60c767fdf75de3ad372efe4c4c0175
jx-dohwan <jx7789@gmail.com>
2025-12-23 12:25:55 +0900

    [UPDATE] 모듈 설정

--- src/core/notification/notification.module.ts
+++ src/core/notification/notification.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
 import { NOTIFICATION_SERVICE } from './notification.interface';
+import { ConfigModule } from '@nestjs/config';
+import { AwsModule } from '../aws/aws.module';
 
 @Module({
+  imports: [
+    ConfigModule, // 2. 여기에 반드시 추가해야 합니다!
+    AwsModule, // (AwsSesService를 쓰기 위해 필요, 이미 Global이면 생략 가능하지만 명시 권장)
+  ],
   providers: [
     {
       // [핵심 설정] 의존성 주입 규칙 정의
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,9 +3,11 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserRepositoryModule } from '../user/repository/user-repository.module';
 import { HashModule } from '../../core/hash/hash.module';
+import { CacheModule } from '../../core/cache/cache.module';
+import { NotificationModule } from '../../core/notification/notification.module';
 
 @Module({
-  imports: [UserRepositoryModule, HashModule],
+  imports: [UserRepositoryModule, HashModule, CacheModule, NotificationModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
